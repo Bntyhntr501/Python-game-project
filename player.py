dadac11d8cbc5c4bedbc3a475ff168c34e3766c9
Bntyhntr501 <bntyhntr501@outlook.com>
2016-03-22 22:01:40 -0400

    Delete player.py

--- player.py
+++
@@ -1,7 +0,0 @@
-from character import *
-
-class player(character):
-    def __init__(self, name, hp, str, int):
-        character.__init__(self, name, hp)
-        self.str = str
-        self.int = int
